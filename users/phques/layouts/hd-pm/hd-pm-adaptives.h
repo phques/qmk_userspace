e99a3e443ac979988253b1ac479e086b5c87efbf
Philippe Quesnel <philippe.quesnel@gmail.com>
2026-03-28 02:12:25 -0400

    organized adaptives file

--- users/phques/layouts/hd-pm/hd-pm-adaptives.h
+++ users/phques/layouts/hd-pm/hd-pm-adaptives.h
@@ -12,50 +12,64 @@ const adaptive_3key_t adaptive_3keys[] PROGMEM = {
 const adaptive_2key_t adaptive_2keys[] PROGMEM = {
     {KC_P, KC_D, "pwd"},                   // pd -> pwd
     {KC_P, KC_F, "ps"},                    // pf -> ps
-    {KC_K, KC_G, "kl"},                    // kg -> kl
-    {KC_M, KC_G, "lg"},                    // mg -> lg
-    {KC_J, KC_G, "jpg"},                   // jg -> jpg
+    {KC_F, KC_P, "sp"},                    // fp -> sp
+
     {KC_W, KC_G, "wn"},                    // wg -> wn
     {KC_W, KC_T, "wd"},                    // wt -> wd
+    {KC_N, KC_H, "nd"},                    // nh -> nd
+
+    {KC_K, KC_G, "kl"},                    // kg -> kl
+    {KC_M, KC_K, "lk"},                    // mk -> lk
     {KC_K, KC_H, "kn"},                    // kh -> kn
+    {KC_H, KC_K, "nk"},                    // hk -> nk
+
+    {KC_J, KC_G, "jpg"},                   // jg -> jpg
     {KC_G, KC_J, "gth"},                   // gj -> gth
+    {KC_G, KC_V, "gt"},                    // gv -> gt
+    {KC_G, KC_M, "gl"},                    // gm -> gl
+    {KC_M, KC_G, "lg"},                    // mg -> lg
+
+    {KC_M, KC_W, "mp"},                    // mw -> mp
+    {KC_W, KC_M, "lm"},                    // wm -> lm
+
+    {KC_V, KC_M, "vl"},                    // vm -> vl
+    {KC_M, KC_V, "lv"},                    // mv -> lv
     {KC_V, KC_J, "vl"},                    // vj -> vl
     {KC_W, KC_J, "wl"},                    // wj -> wl
-    {KC_M, KC_K, "lk"},                    // mk -> lk
-    {KC_H, KC_K, "nk"},                    // hk -> nk
-    {KC_N, KC_H, "nd"},                    // nh -> nd
+
     {KC_P, KC_L, "pl"},                    // pl -> pl
     {KC_B, KC_L, "bl"},                    // bl -> bl
     {KC_S, KC_L, "sl"},                    // sl -> sl
-    {KC_G, KC_M, "gl"},                    // gm -> gl
-    {KC_V, KC_M, "vl"},                    // vm -> vl
-    {KC_W, KC_M, "lm"},                    // wm -> lm
-    {KC_F, KC_P, "sp"},                    // fp -> sp
+
     {KC_D, KC_P, "pg"},                    // dp -> pg
-    {KC_G, KC_V, "gt"},                    // gv -> gt
-    {KC_M, KC_V, "lv"},                    // mv -> lv
     {KC_G, KC_W, "gd"},                    // gw -> gd
-    {KC_M, KC_W, "mp"},                    // mw -> mp
+
     {KC_Y, KC_B, "ib"},                    // yb -> ib
-    {KC_A, KC_E, "au"},                    // ae -> au
+
     {KC_DOT, KC_SLASH, ".com"},            // ./ -> .com
     {KC_DOT, KC_DQUO, ".edu"},             // ." -> .edu
     {KC_DOT, KC_QUOTE, ".org"},            // .' -> .org
+
+    {KC_A, KC_E, "au"},                    // ae -> au
+
     {KC_A, KC_QUOTE, "au"},                // a' -> au
     {KC_U, KC_QUOTE, "ua"},                // u' -> ua
     {KC_E, KC_QUOTE, "eo"},                // e' -> eo
     {KC_O, KC_QUOTE, "oe"},                // o' -> oe
+
     {KC_A, KC_Z, "au"},                    // az -> au
     {KC_U, KC_Z, "ua"},                    // uz -> ua
     {KC_E, KC_Z, "eo"},                    // ez -> eo
     {KC_O, KC_Z, "oe"},                    // oz -> oe
+
     {KC_C, KC_HASH, "cgi"},                // c# -> cgi
     {KC_G, KC_HASH, "gmail"},              // g# -> gmail
     {KC_K, KC_HASH, "quesnel"},            // k# -> quesnel
     {KC_P, KC_HASH, "philippe"},           // p# -> philippe
     {KC_DOT, KC_HASH, "philippe.quesnel"}, // .# -> philippe.quesnel
     {KC_D, KC_HASH, "device"},             // d# -> device
-    {KC_I, KC_HASH, "integration\\"},      // i# -> integration\ (note: trailing backslash is intentional)
+    {KC_I, KC_HASH, "circuit"},            // i# -> circuit
+    {KC_T, KC_HASH, "integration\\"},      // t# -> integration\
     {KC_S, KC_HASH, "select * from "},     // s# -> select * from 
     {0, 0, NULL}
 };
